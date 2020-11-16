8b4d6df9886a6bf0b5f4d1ae08af079fee5b2673
Finn <Finn.Rawles@hotmail.co.uk>
2020-11-16 20:37:03 +0000

    Updated to match main

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const express = require('express');
 const socket = require('socket.io');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
+const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
+const { register, listen } = require('push-receiver');
 const Gpio = require('onoff').Gpio;
 
 let buzzerPin;
+let currentlyConnected = false;
 let rustplus;
 const adapter = new FileSync('db.json');
 const db = low(adapter);
@@ -32,22 +36,27 @@ const server = app.listen(80, function(){
 });
 app.use(express.static('public'));
 
+app.use(express.static('public', {
+    extensions: 'html'
+}));
+
 if (db.get('notificationSettingsGPIOPin').value() !== '') {
   buzzerPin = new Gpio(parseInt(db.get('notificationSettingsGPIOPin').value()), 'out');
 }
 
 const io = socket(server);
 io.on('connection', (socket) => {
-    db.get('devices').value().forEach(smartDevice => {
-      socket.emit('server_UpdateDevice', smartDevice);
-      rustplus.getEntityInfo(smartDevice.deviceID, (message) => {
-        return true;
+    if (currentlyConnected) {
+      db.get('devices').value().forEach(smartDevice => {
+        socket.emit('server_UpdateDevice', smartDevice);
+        rustplus.getEntityInfo(smartDevice.deviceID, (message) => {
+          return true;
+        });
       });
-    });
+    }
 
-    socket.emit('server_UpdateServerSettings', {"hostname": db.get('severSettingsHostname').value()});
+    socket.emit('server_UpdateServerSettings', {"hostname": db.get('severSettingsHostname').value(), "connectionState": currentlyConnected});
     socket.emit('server_UpdateNotificationSettings', {
-      "GPIOPin": db.get('notificationSettingsGPIOPin').value(),
       "discordWebhookLink": db.get('notificationSettingsDiscordURL').value(),
       "discordMessage": db.get('notificationSettingsDiscordMessage').value()}
     );
@@ -148,16 +157,16 @@ io.on('connection', (socket) => {
     });
 
     socket.on('client_UpdateNotificationSettings', function(data){
-      db.set('notificationSettingsGPIOPin', data.GPIOPin).write();
       db.set('notificationSettingsDiscordURL', data.discordWebhookLink).write();
+      db.set('notificationSettingsDiscordMessage', data.discordMessage).write();
       db.set('notificationSettingsDiscordMessage', data.discordMessage).write();
 
       buzzerPin = new Gpio(parseInt(db.get('notificationSettingsGPIOPin').value()), 'out');
 
       io.sockets.emit('server_UpdateNotificationSettings', {
-        "GPIOPin": db.get('notificationSettingsGPIOPin').value(),
         "discordWebhookLink": db.get('notificationSettingsDiscordURL').value(),
-        "discordMessage": db.get('notificationSettingsDiscordMessage').value()}
+        "discordMessage": db.get('notificationSettingsDiscordMessage').value(),
+        "GPIOPin": db.get('notificationSettingsGPIOPin').value()}
       );
     });
 
@@ -166,11 +175,94 @@ io.on('connection', (socket) => {
       db.set('severSettingsRustPlusPort', data.rustPlusPort).write();
       db.set('severSettingsSteamID', data.steamID).write();
       db.set('serverSettingsPlayerToken', data.playerToken).write();
+
       connectToRustPlus();
 
-      io.sockets.emit('server_UpdateServerSettings', {"hostname": db.get('severSettingsHostname').value()});
+      io.sockets.emit('server_UpdateServerSettings', {"hostname": db.get('severSettingsHostname').value(), "connectionState": currentlyConnected});
+  });
+
+  // Stuff for pair.js
+
+  socket.on('pair_client_connect', function(data){ // NEED TO FINISH.
+    pairClientConnect({'socketID': socket.id, 'connectedFrom': data.connectedFrom});
+  });
+
+  socket.on('pair_server_disconnect', function(data) {
+    shutdown(data.steamAuthToken, data.expoPushToken);
   });
 });
+
+async function pairClientConnect(data) {
+  const credentials = await register('976529667804');
+  axios.post('https://exp.host/--/api/v2/push/getExpoPushToken', {
+      deviceId: uuidv4(),
+      experienceId: '@facepunch/RustCompanion',
+      appId: 'com.facepunch.rust.companion',
+      deviceToken: credentials.fcm.token,
+      type: 'fcm',
+      development: false,
+  }).then(async (response) => {
+
+      expoPushToken = response.data.data.expoPushToken;
+
+      app.get(`/pair/${data.socketID}`, (req, res) => {
+
+        steamAuthToken = req.query.token;
+
+        io.to(data.socketID).emit('pair_server_deleteOnLeave',{'steamAuthToken': steamAuthToken, 'expoPushToken': expoPushToken});
+
+        axios.post('https://companion-rust.facepunch.com:443/api/push/register', {
+            AuthToken: steamAuthToken,
+            DeviceId: 'rustplus-api',
+            PushKind: 0,
+            PushToken: expoPushToken,
+        }).then((response) => {
+            io.to(data.socketID).emit('pair_server_synced',{});
+            waitForRustPlusInfo(credentials, data.socketID);
+        }).catch((error) => {
+            console.log("Failed to register with Rust Companion API");
+            console.log(error);
+        });
+
+        res.sendFile('public/success.html', {root: __dirname })
+      });
+      const url = "https://companion-rust.facepunch.com/login?returnUrl=" + encodeURIComponent(`${data.connectedFrom}/${data.socketID}?expoPushToken=expoPushToken`);
+      io.to(data.socketID).emit('pair_server_popup', { 'url': url });
+
+  }).catch((error) => {
+      console.log("Failed to fetch Expo Push Token");
+      console.log(error);
+  });
+}
+
+async function waitForRustPlusInfo(credentials, socketID){
+  await listen(credentials, ({ notification, persistentId }) => {
+      const body = JSON.parse(notification.data.body);
+
+      if (body.type === 'server') {
+        io.to(socketID).emit('pair_server_rustplusinfo', { 'type': body.type,'ip': body.ip, 'port': body.port, 'playerToken': body.playerToken, 'steamID': body.playerId});
+      } else {
+        io.to(socketID).emit('pair_server_rustplusinfo', { 'type': body.type, 'deviceID': body.entityId, 'deviceType': body.entityName});
+      }
+  });
+}
+
+async function shutdown(steamAuthToken, expoPushToken) {
+    // unregister with Rust Companion API
+    if(steamAuthToken){
+        await axios.delete('https://companion-rust.facepunch.com:443/api/push/unregister', {
+            data: {
+                AuthToken: steamAuthToken,
+                PushToken: expoPushToken,
+                DeviceId: 'rustplus-api',
+            },
+        }).then((response) => {
+
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+}
 
 function connectToRustPlus(){
   const hostname = db.get('severSettingsHostname').value();
@@ -179,9 +271,21 @@ function connectToRustPlus(){
   const playerToken = db.get('serverSettingsPlayerToken').value();
 
   rustplus = new RustPlus(hostname, rustPlusPort, steamID, playerToken);
-}
 
-rustplus.on('connected', () => { // Only here temporarily.
+
+rustplus.on('error', e => {
+  currentlyConnected = false;
+  io.sockets.emit('server_UpdateServerSettings', {"hostname": db.get('severSettingsHostname').value(), "connectionState": currentlyConnected});
+
+  db.get('devices').value().forEach(smartDevice => {
+    smartDevice.deviceState = 'No Response';
+    io.sockets.emit('server_UpdateDevice', smartDevice);
+  });
+});
+
+rustplus.on('connected', () => {
+    currentlyConnected = true;
+    io.sockets.emit('server_UpdateServerSettings', {"hostname": db.get('severSettingsHostname').value(), "connectionState": currentlyConnected});
     rustplus.sendTeamMessage(`Hello! Server initialised at ${new Date()}`);
 
     db.get('devices').value().forEach(smartDevice => {
@@ -197,6 +301,7 @@ rustplus.on('connected', () => { // Only here temporarily.
             .assign({ deviceState: message.response.entityInfo.payload.value ? 'Active': 'Inactive'})
             .write()
         }
+        io.sockets.emit('server_UpdateDevice', smartDevice);
         return true;
       });
     });
@@ -211,8 +316,8 @@ rustplus.on('message', (message) => {
         .write();
       io.emit('server_UpdateDevice',db.get('devices').find({ deviceID: entityChanged.entityId }).value());
 
-
       if (db.get('devices').find({ deviceID: entityChanged.entityId }).value().deviceState === 'Inactive') { return; }
+
       if (db.get('devices').find({ deviceID: entityChanged.entityId }).value().notificationDiscord ) {
         const hook = new Webhook(db.get('notificationSettingsDiscordURL').value());
         const IMAGE_URL = 'https://static.wikia.nocookie.net/play-rust/images/0/06/Rocket_Launcher_icon.png';
@@ -227,6 +332,7 @@ rustplus.on('message', (message) => {
       }
     }
 });
+}
 
 async function runBuzzer() {
   for(let i = 0; i < 4; i++) {
